bfe4a66111912e23b588968c21bdb376330177d2
vishal.Alok <you@example.com>
2021-10-17 19:12:14 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Deault_Constructor
+{
+    using System;
+    namespace DefaultConstructorExample
+    {
+        class Geek
+        {
+            int num;
+            string name;
+            Geek()
+            {
+                Console.WriteLine("Constructor Called and feilds are assigned with default values");
+            }
+            public static void Main()
+            {
+                Geek geek1 = new Geek();
+                Console.WriteLine(geek1.name);
+                Console.WriteLine(geek1.num);
+            }
+        }
+    }
+}
